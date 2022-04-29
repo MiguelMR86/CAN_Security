8fe8be06993797f792ec11048071e53a0b78cb63
Miriam Beatriz Curbelo Lorenzo <miriiamx3@gmail.com>
2022-04-29 16:46:29 +0000

    Start Funciones

---
+++ funciones.sql
@@ -0,0 +1,6 @@
+-- Aitor
+
+-- Función a la que se le pasa un código de Estante y que 
+-- devuelva los Productos que estén en Stock, con su nombre 
+-- y la cantidad del mismo siguiendo el siguiente formato: 
+-- "Los productos en stock son: Switch POE: 10 unidades, Cable UTP: 3 unidades..."
