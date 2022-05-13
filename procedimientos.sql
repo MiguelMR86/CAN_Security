6c0486e6865231d1551ed00997627174d2eb2402
Sstark97 <aitorscinfo@gmail.com>
2022-05-13 11:19:25 +0100

    Change First Procedure

--- procedimientos.sql
+++ procedimientos.sql
@@ -14,11 +14,11 @@ BEGIN
     SELECT Stock INTO cantidadProducto FROM Estante WHERE NombreEst = codEstanteRetirar AND CodProducto = codProdRetirar;
 
     IF codEstanteRetirar NOT IN (SELECT NombreEst FROM Estante) THEN 
-        SELECT "No existe ese Estante";
+        SELECT "No existe ese Estante" ERR1;
     ELSEIF codProdRetirar NOT IN (SELECT CodProducto FROM Producto) THEN
-        SELECT "El Producto no existe";
+        SELECT "El Producto no existe" ERR2;
     ELSEIF cantidadRetirar <= 0 THEN 
-        SELECT "La Cantidad a retirar debe ser mayor a 0";
+        SELECT "La Cantidad a retirar debe ser mayor a 0" ERR3;
     ELSEIF cantidadProducto > cantidadRetirar THEN 
         SET cantidadProducto = cantidadProducto - cantidadRetirar;
 
